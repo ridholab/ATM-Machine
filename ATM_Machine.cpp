8f4ba6d72d023699efaf6baafad50ec804ce59c6
Ridho Mulyaworo <ridholab@gmail.com>
2024-10-26 16:18:42 +0900

    Update ATM_Machine.cpp

--- ATM_Machine.cpp
+++ ATM_Machine.cpp
@@ -29,33 +29,26 @@ void atm_machine_transaction(){
             cout << "\nMasukkan Jumlah Setoran Tunai: ";
             cin >> jumlah_setoran;
             jumlah_saldo += jumlah_setoran;
-
-
+            
             cout << "Saldo Akhir Anda Rp. " << jumlah_saldo << ". Thanks for depositing!" << endl;
             cout << "Apakah Anda Ingin Melakukan Transaksi yang Lain?\nTekan 1 untuk 'Memproses' dan 2 untuk 'Keluar'\n" << endl;
-            
-            
+              
             cout << "Your option: ";
             cin >> transaksi_baru;
 
             if (transaksi_baru == 1){
-
                 atm_machine_transaction();
-
             }
-
         break;
 
         // Case untuk Tarik Tunai
         case 2:
-
+            
             cout << "\nMasukkan Jumlah Penarikan Tunai: ";
             cin >> jumlah_penarikan;
 
             if (jumlah_penarikan > jumlah_saldo)
-
             {
-
                 cout << "Saldo Tidak Mencukupi! Silahkan Melakukan Proses Setor Tunai Terlebih Dahulu." << endl;
                 cout << "Apakah Anda Ingin Melakukan Transaksi yang Lain?\nTekan 1 untuk 'Memproses' dan 2 untuk 'Keluar'\n"<< endl;
 
@@ -63,17 +56,12 @@ void atm_machine_transaction(){
                 cin >> transaksi_baru;
 
                 if (transaksi_baru == 1){
-
                     atm_machine_transaction();
-
                 }
 
             }
-
             else
-
             {
-
                 jumlah_saldo -= jumlah_penarikan;
 
                 cout << "Anda Telah Melakukan Penarikan Tunai Rp. " << jumlah_penarikan << " dan Sisa Saldo Anda Rp. " << jumlah_saldo << endl;
@@ -83,19 +71,13 @@ void atm_machine_transaction(){
                 cin >> transaksi_baru;
 
                 if (transaksi_baru == 1){
-
                     atm_machine_transaction();
-
                 }
-
             }
-
         break;
 
         // Case untuk Cek Saldo
-
         case 3:
-
             cout << "Sisa Saldo Anda Rp. " << jumlah_saldo << endl;
             cout << "Apakah Anda Ingin Melakukan Transaksi yang Lain?\nTekan 1 untuk 'Memproses' dan 2 untuk 'Keluar'\n" << endl;
 
@@ -103,27 +85,15 @@ void atm_machine_transaction(){
             cin >> transaksi_baru;
 
             if (transaksi_baru == 1){
-
                 atm_machine_transaction();
-
             }
-
         break;
-
     }
-
 }
 
-
-
 int main()
-
 {
-
     atm_machine_transaction();
-
     cout << "Terima Kasih Telah Melakukan Transaksi!";
-
     return 0;
-
 }
